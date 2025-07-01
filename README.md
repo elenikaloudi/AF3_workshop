18936fa4ddcd022634eaca35ad57079af46e1591
elenikaloudi <s4178858@nodelogin01.cm.cluster>
2025-07-01 12:51:57 +0200

    pushagain

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# AF3_workshop
-Here you can find all the steps necessary to follow the workshop. 
+# 🧬 Welcome to the AlphaFold Workshop: Protein-Protein Interaction Predictions
+
+Hello and welcome! 👋  
+
+This repository is part of a hands-on workshop on using `AlphaFold` to predict **protein-protein interactions**. In this guide, you'll learn how to:
+
+- Collect protein sequences
+- Prepare them for multimer predictions
+- Run AlphaFold on the server and on the HPC
+- Analyze the results and structure quality
+
+Whether you're a beginner in structural biology or just trying to automate your protein interaction screening, this walkthrough is for you!
+
+---
+
+
+## 🚀 Step 1: Get Your Protein Sequences
+
+You can get protein sequences from [UniProt] (https://www.uniprot.org/) 
---
+++ dram2_test_json/Q91YT8_with_Q9CR48/batch_Q91YT8_with_Q9CR48.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name=Q91YT8_with_Q9CR48
+#SBATCH --partition=gpu-short
+#SBATCH --mem=32GB
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=A100.4g.40gb|A100.3g.40gb
+#SBATCH --cpus-per-task=8
+#SBATCH --time=03:30:00
+#SBATCH --output="Q91YT8_with_Q9CR48_%j.log"
+#SBATCH --error="Q91YT8_with_Q9CR48_%j.err"
+#SBATCH --mail-user=e.kaloudi@umail.leidenuniv.nl
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+
+echo "#### Running on nodelogin01"
+
+echo "#### Loading module"
+module load alphafold/cc8_3-20250304
+
+echo "#### Checking GPU"
+nvidia-smi
+
+echo "#### Running alphafold"
+
+export AF3_RESOURCES_DIR=/data1/databases/AlphaFold3_resources
+export AF3_INPUT_DIR=/home/s4178858/data1/alphafold/AFSJson/dram2_test/Q91YT8_with_Q9CR48/
+export AF3_OUTPUT_DIR=/home/s4178858/data1/alphafold/output_dram2_test/Q91YT8_with_Q9CR48
+export AF3_MODEL_PARAMETERS_DIR=${AF3_RESOURCES_DIR}/weights
+export AF3_DATABASES_DIR=${AF3_RESOURCES_DIR}/databases
+
+alphafold \
+        --db_dir=${AF3_DATABASES_DIR} \
+        --model_dir=${AF3_MODEL_PARAMETERS_DIR} \
+        --input_dir=${AF3_INPUT_DIR} \
+        --output_dir=${AF3_OUTPUT_DIR}
+
+echo "#### Finished"
